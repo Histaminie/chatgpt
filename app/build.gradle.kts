edf1b1e3b864e97007b90e9218ca55042b43fa9f
ajaygirigoswami <ajaygswm14@gmail.com>
2026-04-03 21:16:19 +0530

    updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,10 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.navigation:navigation-compose:2.8.6")
+
+    implementation("com.google.android.material:material:1.13.0")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
